8387d9cc289c428eb74899f6046c35fe23900072
AdamMeyers42 <adammeyers3551@gmail.com>
2021-11-22 22:42:43 -0700

    Created Getallproducts, GetproductbyId, and CreateProduct

---
+++ eCommerceStarterCode/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Security.Claims;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet, Authorize]
+        public IActionResult GetAllProducts()
+        {
+            var products = _context.Products.ToList();
+
+            return Ok(products);
+        }
+
+
+        [HttpGet("{id}"), Authorize]
+
+        public IActionResult GetProductById(int id)
+        {
+            var product = _context.Products.Where(p => p.Id == id).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateProduct([FromBody]Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
